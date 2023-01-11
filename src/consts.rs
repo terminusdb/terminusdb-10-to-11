43fa1afd7166073b274108543daa7abd278d4272
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-01-11 18:01:38 +0100

    do not copy predicate idmap twice

--- src/consts.rs
+++ src/consts.rs
@@ -284,7 +284,7 @@ pub const V11_FILENAMES: Filenames = Filenames {
     rollup: "rollup.hex",
 };
 
-pub const UNCHANGED_FILES: [&'static str; 41] = [
+pub const UNCHANGED_FILES: [&'static str; 38] = [
     V10_FILENAMES.predicate_idmap_bits,
     V10_FILENAMES.predicate_idmap_bit_index_blocks,
     V10_FILENAMES.predicate_idmap_bit_index_sblocks,
@@ -323,9 +323,6 @@ pub const UNCHANGED_FILES: [&'static str; 41] = [
     V10_FILENAMES.neg_predicate_wavelet_tree_bit_index_sblocks,
     V10_FILENAMES.pos_subjects,
     V10_FILENAMES.neg_subjects,
-    V10_FILENAMES.predicate_idmap_bits,
-    V10_FILENAMES.predicate_idmap_bit_index_blocks,
-    V10_FILENAMES.predicate_idmap_bit_index_sblocks,
 ];
 
 pub const BASE_NAIVE_UNCHANGED_FILES: [&'static str; 8] = [
