904bf9274ddac6fa5e786a939591e9154d3c70c8
Matthijs van Otterdijk <matthijs@terminusdb.com>
2023-02-14 10:35:09 +0100

    Unescape legacy prolog strings during conversion

--- src/dataconversion.rs
+++ src/dataconversion.rs
@@ -98,7 +98,8 @@ pub fn convert_value_string_to_dict_entry(value: &str) -> Result<tfc_11::TypedDi
     let res = value_string_to_slices(value)?;
     Ok(match res {
         LangOrType::Lang(s, l) => {
-            <LangString as tfc_11::TdbDataType>::make_entry(&format!("{l}@{s}"))
+            let converted = prolog_string_to_string(s);
+            <LangString as tfc_11::TdbDataType>::make_entry(&format!("{l}@{converted}"))
         }
         LangOrType::Type(s, t) => {
             if t == "http://www.w3.org/2001/XMLSchema#boolean" {
@@ -227,7 +228,8 @@ pub fn convert_value_string_to_dict_entry(value: &str) -> Result<tfc_11::TypedDi
                 let slice = &s[1..s.len() - 1];
 
                 if t == "http://www.w3.org/2001/XMLSchema#string" {
-                    <String as tfc_11::TdbDataType>::make_entry(&slice)
+                    let converted = prolog_string_to_string(slice);
+                    <String as tfc_11::TdbDataType>::make_entry(&converted)
                 } else if t == "http://www.w3.org/2001/XMLSchema#time" {
                     let nt: NaiveTime =
                         NaiveTime::parse_from_str(slice, "%H:%M:%S%.f%Z").map_err(|_| {
@@ -570,6 +572,71 @@ pub fn normalize_decimal(s: &str) -> std::result::Result<Cow<str>, DecimalValida
         Err(DecimalValidationError {
             value: s.to_string(),
         })
+    }
+}
+
+const SWIPL_CONTROL_CHAR_A: char = 7 as char;
+const SWIPL_CONTROL_CHAR_B: char = 8 as char;
+const SWIPL_CONTROL_CHAR_F: char = 12 as char;
+const SWIPL_CONTROL_CHAR_V: char = 11 as char;
+
+fn prolog_string_to_string(s: &str) -> Cow<str> {
+    let mut result: Option<String> = None;
+    let mut escaping = false;
+    let mut characters = s.char_indices().skip(1);
+    while let Some((ix, c)) = characters.next() {
+        if escaping {
+            let result = result.as_mut().unwrap();
+            match c {
+                '\\' => result.push('\\'),
+                '\"' => result.push('\"'),
+                'x' => result.push(unescape_legacy_prolog_escape_sequence(&mut characters)),
+                'a' => result.push(SWIPL_CONTROL_CHAR_A),
+                'b' => result.push(SWIPL_CONTROL_CHAR_B),
+                't' => result.push('\t'),
+                'n' => result.push('\n'),
+                'v' => result.push(SWIPL_CONTROL_CHAR_V),
+                'f' => result.push(SWIPL_CONTROL_CHAR_F),
+                'r' => result.push('\r'),
+                _ => panic!("unknown prolog escape code in string"),
+            }
+
+            escaping = false;
+        } else {
+            if c == '\\' {
+                escaping = true;
+                if result.is_none() {
+                    let mut r = String::with_capacity(s.len());
+                    r.push_str(&s[1..ix]);
+                    result = Some(r);
+                }
+            } else if let Some(result) = result.as_mut() {
+                result.push(c);
+            }
+        }
+    }
+
+    match result {
+        Some(mut result) => {
+            assert_eq!(Some('\"'), result.pop());
+            Cow::Owned(result)
+        }
+        None => Cow::Borrowed(&s[1..s.len() - 1]),
+    }
+}
+
+fn unescape_legacy_prolog_escape_sequence(
+    characters: &mut impl Iterator<Item = (usize, char)>,
+) -> char {
+    let mut digits: String = String::new();
+    loop {
+        let (_, digit) = characters.next().unwrap();
+        if digit == '\\' {
+            let hex = u32::from_str_radix(&digits, 16).unwrap();
+            return char::from_u32(hex).unwrap();
+        } else {
+            digits.push(digit);
+        }
     }
 }
 
